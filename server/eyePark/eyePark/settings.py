9fa5e89a5aa2dcd5ed99221d2e1f3beb82337839
Juan Rojas <rojasjuandiego6@gmail.com>
2021-10-30 16:59:55 -0400

    Add Secret key to env variable

--- server/eyePark/eyePark/settings.py
+++ server/eyePark/eyePark/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v-)t)9-^n$bo^7u^b^8@hd^_njm=$&-=pu!o@4gxpb*e@ftzel'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'eyePark.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'eyePark.sqlite3',
     }
 }
 
