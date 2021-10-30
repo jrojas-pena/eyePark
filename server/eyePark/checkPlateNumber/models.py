660be0ca7131a185193c62b7b291a934b937a881
Juan Rojas <rojasjuandiego6@gmail.com>
2021-10-30 19:57:54 -0400

    added db models

--- server/eyePark/checkPlateNumber/models.py
+++ server/eyePark/checkPlateNumber/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+class Parking_Lot(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=200)
+
+class Car(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    license_plate = models.CharField(max_length=15)
+    model = models.CharField(max_length=50)
+    colour = models.CharField(max_length=10)
