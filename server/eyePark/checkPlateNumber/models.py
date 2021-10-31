e7648d1572fa8b74459d1a5a1bf18ddc63a14a67
Juan Rojas <rojasjuandiego6@gmail.com>
2021-10-30 20:07:06 -0400

    added str functions to classes in models, added parking lot number field to parking lot class

--- server/eyePark/checkPlateNumber/models.py
+++ server/eyePark/checkPlateNumber/models.py
@@ -7,10 +7,17 @@ from django.contrib.auth.models import User
 
 class Parking_Lot(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    number = models.IntegerField(default=0)
     location = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.location + " " + self.number
 
 class Car(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     license_plate = models.CharField(max_length=15)
     model = models.CharField(max_length=50)
     colour = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.license_plate
