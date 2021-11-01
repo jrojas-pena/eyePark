4aae8b45b0ddafb3529adb0fbd454efcfd4cdb58
Juan Rojas <rojasjuandiego6@gmail.com>
2021-11-01 15:43:52 -0400

    changed ALLOWED_HOST

--- server/eyePark/eyePark/settings.py
+++ server/eyePark/eyePark/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', '192.168.0.100', 'cafeprogramming.ddns.net']
 
 
 # Application definition
