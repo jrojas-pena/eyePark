86386d151b3ff9d46d76e38441bc795c8bc51c02
Juan Rojas <rojasjuandiego6@gmail.com>
2021-11-12 17:59:10 -0500

    added host to allowed host, removed csrf token method

--- server/eyePark/checkPlateNumber/views.py
+++ server/eyePark/checkPlateNumber/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotFound
 from .models import Car, Parking_Lot
-from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie, requires_csrf_token
+from django.views.decorators.csrf import csrf_exempt
 import json
 from django.contrib.auth.models import User
 # Create your views here.
 
+@csrf_exempt
 def checkPlateNumber(request):
     data = json.loads(request.body)
     parkingLot = get_object_or_404(Parking_Lot,pk=int(data['parking-lot-number']))
@@ -14,16 +15,13 @@ def checkPlateNumber(request):
     print("Car with license plate: %s" % car)
     return HttpResponse()
 
+@csrf_exempt
 def addPlateNumber(request):
     data = json.loads(request.body)
     parkingLot = get_object_or_404(Parking_Lot,pk=int(data['parking-lot-number']))
     user = parkingLot.user
     car = Car(user=user, license_plate=data['license-plate'])
     car.save()
-    return HttpResponse()
-
-@ensure_csrf_cookie
-def giveCsrfToken(request):
     return HttpResponse()
 
 
