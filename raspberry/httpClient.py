5e5718ef52d31f27e6cd165451403faeafd1b427
Juan Rojas <rojasjuandiego6@gmail.com>
2021-11-20 20:26:10 -0500

    remove unnecessary code

--- raspberry/httpClient.py
+++ raspberry/httpClient.py
@@ -14,19 +14,3 @@ def addPlateNumber(data, config):
         r = client.post(json=data, url=url)
         return r.status_code == 200
 
-
-# f = open("/home/juan/eyePark/raspberry/config/system_config.json", "r")
-# config = json.loads(f.read())
-# f.close()
-# data = {
-#     "parking-lot-number" : 52,
-#     "license-plate" : "abc123"
-# }
-# print(checkPlateNumber(data, config))
-# print(addPlateNumber(data, config))
-# with httpx.Client() as client:
-#     url = config["server_url"]  + ':%d' % config["server_port"]
-#     # csrfToken = requestCsrfToken(url + config["get_csrf_path"], client)
-#     postRequest(data, url + config["check_plate_path"],  client)
-#     postRequest(data, url + config["add_plate_path"], client)
-    
