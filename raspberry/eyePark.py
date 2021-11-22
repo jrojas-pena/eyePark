c69a7b881df54b588f07af2e3c5204cfd3f5d622
Juan Rojas <rojasjuandiego6@gmail.com>
2021-11-22 14:34:17 -0500

    added plate_reader class, changed httpConfig to class, created eyePark main program

---
+++ raspberry/eyePark.py
@@ -0,0 +1,6 @@
+from .http_eyepark_client import http_eyepark_client
+from .plate_reader import plate_reader
+import json
+import datetime
+
+
---
+++ raspberry/http_eyepark_client.py
@@ -0,0 +1,15 @@
+import httpx
+
+class http_eyepark_client(httpx.Client):
+    def checkPlateNumber(data, config):
+        with httpx.Client() as client:
+            url = '%s:%d%s%d/%s/' % (config["server_url"], config["server_port"], config["check_plate_path"], data['parking-lot-number'], data['license-plate'])
+            r = client.get(url)
+            return r.status_code == 200
+
+    def addPlateNumber(data, config):
+        with httpx.Client() as client:
+            url = config["server_url"]  + ':%d' % config["server_port"] + config["add_plate_path"]
+            r = client.post(json=data, url=url)
+            return r.status_code == 200
+
