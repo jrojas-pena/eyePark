3130c09e15a440e7aa1df6e3d74a83d67000d31b
Juan Rojas <rojasjuandiego6@gmail.com>
2021-11-25 00:29:36 -0500

    More organized

--- server/eyePark/checkPlateNumber/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('<int:parking-spot>/<string:license-plate>/', views.checkPlateNumber, name='check-plate'),
-    path('add-plate/', views.addPlateNumber, name='add-plate')
-]
