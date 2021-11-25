95ec5928db4c584a24a2898edaf7ff0440f7c837
Juan Rojas <rojasjuandiego6@gmail.com>
2021-11-25 00:47:21 -0500

    fixed requests

--- src/raspberry/http_eyepark_client.py
+++ src/raspberry/http_eyepark_client.py
@@ -1,18 +1,18 @@
 import requests
 
-class http_eyepark_client(requests):
+class http_eyepark_client():
 
     def __init__(self, config):
-        super().__init__()
         self.config = config
+
 
     def checkPlateNumber(self, data):
         url = '%s:%d%s%d/%s/' % (self.config["server_url"], self.config["server_port"], self.config["check_plate_path"], data['parking-lot-number'], data['license-plate'])
-        r = self.get(url)
+        r = requests.get(url)
         return r.status_code == 200
 
     def addPlateNumber(self, data):
         url = self.config["server_url"]  + ':%d' % self.config["server_port"] + self.config["add_plate_path"]
-        r = self.post(json=data, url=url)
+        r = requests.post(json=data, url=url)
         return r.status_code == 200
 
